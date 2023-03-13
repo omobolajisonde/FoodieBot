5cd66e06865ddb23ee30316cc1a96e7bbe5441ad
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2023-03-13 11:59:13 +0100

    fix: solved render deploy issue

--- public/config.js
+++ public/config.js
@@ -1,1 +1,2 @@
-export const WEB_SOCKET_URL = "http://localhost:5000";
+// export const WEB_SOCKET_URL = "http://localhost:5000";
+export const WEB_SOCKET_URL = "https://foodiebot-4wzo.onrender.com/";
